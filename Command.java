7efcd0e30518839e991699d81eb6e21c5ee787b8
ArifRey10 <143960781+ArifRey10@users.noreply.github.com>
2023-10-22 20:12:51 -0400

    Add files via upload

--- Command.java
+++ Command.java
@@ -13,39 +13,37 @@
  *
  * If the command had only one word, then the second word is <null>.
  * 
- * @author  Michael Kölling and David J. Barnes
- * @version 2016.02.29
+ * @author  Arif Reyhan
+ * @version 10/22/2023
  */
 
 public class Command
 {
-    private CommandWord commandWord;
+    private String commandWord;
     private String secondWord;
 
     /**
-     * Create a command object. First and second words must be supplied, but
-     * the second may be null.
-     * @param commandWord The CommandWord. UNKNOWN if the command word
-     *                  was not recognised.
-     * @param secondWord The second word of the command. May be null.
+     * Create a command object. First and second word must be supplied, but
+     * either one (or both) can be null. The command word should be null to
+     * indicate that this was a command that is not recognised by this game.
      */
-    public Command(CommandWord commandWord, String secondWord)
+    public Command(String firstWord, String secondWord)
     {
-        this.commandWord = commandWord;
+        commandWord = firstWord;
         this.secondWord = secondWord;
     }
 
     /**
-     * Return the command word (the first word) of this command.
-     * @return The command word.
+     * Return the command word (the first word) of this command. If the
+     * command was not understood, the result is null.
      */
-    public CommandWord getCommandWord()
+    public String getCommandWord()
     {
         return commandWord;
     }
 
     /**
-     * @return The second word of this command. Returns null if there was no
+     * Return the second word of this command. Returns null if there was no
      * second word.
      */
     public String getSecondWord()
@@ -54,15 +52,15 @@ public class Command
     }
 
     /**
-     * @return true if this command was not understood.
+     * Return true if this command was not understood.
      */
     public boolean isUnknown()
     {
-        return (commandWord == CommandWord.UNKNOWN);
+        return (commandWord == null);
     }
 
     /**
-     * @return true if the command has a second word.
+     * Return true if the command has a second word.
      */
     public boolean hasSecondWord()
     {
